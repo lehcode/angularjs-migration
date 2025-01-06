9a9503a517eb2da37b198549d3f3328395ca294a
lehcode <53556648+lehcode@users.noreply.github.com>
2025-01-06 11:47:06 +0200

    feat: Dashboard component

--- apps/barq/src/app/app.routes.ts
+++ apps/barq/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Route } from '@angular/router';
 import { LoginComponent } from './components/login.component';
+import { DashboardComponent } from './components/dashboard.component';
 
 export const appRoutes: Route[] = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
   }
 ];
---
+++ apps/barq/src/app/components/dashboard.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: 'app-dashboard',
+  template: `<h1>Dashboard</h1>`,
+
+})
+export class DashboardComponent implements OnInit {
+
+  // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
+  ngOnInit(): void {
+      //
+  }
+}
